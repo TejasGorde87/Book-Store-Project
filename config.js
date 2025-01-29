e6172bb2b62ea044feef1d897b5f2a6d64d090c2
Tejas Gorde <61644593+TejasGorde87@users.noreply.github.com>
2025-01-29 17:08:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://tejasgorde:Tejasxyz%40%40123@book-store-mern.rbym3.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern'
